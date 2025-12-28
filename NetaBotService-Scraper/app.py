4d72a672b9004055fd348d82e9e137420d1dfcc9
Muhammad Dzulfiqar <69383545+dzulfiqar03@users.noreply.github.com>
2025-12-28 15:31:29 +0700

    Remove main execution block from app.py
    Remove main execution block from app.py

--- NetaBotService-Scraper/app.py
+++ NetaBotService-Scraper/app.py
@@ -113,7 +113,3 @@ def scrape():
         "status": "success",
         "total": count
     })
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)
